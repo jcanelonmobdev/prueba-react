7e2a3589e053c74ce9718c4b12a205e2ed101650
Julio César Canelon Rangel <jcanelon@mobdev.cl>
2021-03-08 16:10:00 -0300

    Clase 28 Array de objetos

--- my-app/src/components/Listas.jsx
+++ my-app/src/components/Listas.jsx
@@ -2,7 +2,14 @@ import React, { useState} from 'react'
 
 const Listas = () => {
 
-    const initialState = [0, 1, 2, 3, 4]
+    const initialState = [
+        {id: 1, texto: "Tarea 1"},
+        {id: 2, texto: "Tarea 2"},
+        {id: 3, texto: "Tarea 3"},
+        {id: 4, texto: "Tarea 4"},
+        {id: 5, texto: "Tarea 5"},
+    ]
+
     const [lista, setlista] = useState(initialState)
 
     return (
@@ -10,7 +17,7 @@ const Listas = () => {
             <h2>Listas</h2>
             {
                 lista.map((item, index) => (
-                    <h4 key={ index }>{ item }</h4>
+                    <h4 key={ item.id }>{ item.texto }</h4>
                 ))
             }
         </div>
