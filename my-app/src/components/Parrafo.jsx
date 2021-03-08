eb66d84e6a320897c1f3996bda492040643f5921
Julio César Canelon Rangel <jcanelon@mobdev.cl>
2021-03-08 11:56:52 -0300

    Clase 20

---
+++ my-app/src/components/Parrafo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Parrafo = () => {
+    return (
+        <div>
+             <p>Bienvenido a mi primer sitio web</p>
+        </div>
+    )
+}
+
+export default Parrafo
