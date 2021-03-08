034e2e3bf9d46409cc99a4d0c6b60f41eae340ae
Julio César Canelon Rangel <jcanelon@mobdev.cl>
2021-03-08 11:56:41 -0300

    Clase 20

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Parrafo from './components/Parrafo'
 
 function App() {
   return (
     <div>
       <h1>Hola Mundo React</h1>
+      <Parrafo />
+      <Parrafo /><Parrafo />
     </div>
   );
 }
