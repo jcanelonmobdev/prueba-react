1450b016efbd92de5870aaded3ab0689270a8a77
Julio César Canelon Rangel <jcanelon@mobdev.cl>
2021-03-08 15:03:05 -0300

    Clase 26 Operador ternario

--- my-app/src/components/Contador.jsx
+++ my-app/src/components/Contador.jsx
@@ -8,6 +8,11 @@ const Contador = () => {
         <div>
             <h2>Contador</h2>
             <h3>Nuestro numero: { contador }</h3>
+            <h4>
+                {
+                    contador > 2 ? 'NO es mayor a 2' : 'Es menor a dos'
+                }
+            </h4>
             <button onClick= { () =>  setContador(contador + 1)} > Aumentar </button>
             <button onClick= { () =>  setContador(0)} > Resetear </button>
         </div>
