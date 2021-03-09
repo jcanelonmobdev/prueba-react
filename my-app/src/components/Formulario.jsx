9315649486ead2392af3b1e00798238bbcfd618a
Julio César Canelon Rangel <jcanelon@mobdev.cl>
2021-03-08 21:29:05 -0300

    Formularios en React

--- my-app/src/components/Formulario.jsx
+++ my-app/src/components/Formulario.jsx
@@ -1,21 +1,47 @@
 import React from 'react'
 
 const Formulario = () => {
+
+    const [fruta, setFruta] = React.useState('')
+    const [descripcion, setDescripcion] = React.useState('')
+
+    const guardarDatos = (e) => {
+        e.preventDefault();
+
+        if (!fruta.trim()){
+            console.log("esta vacio fruta")
+            return
+        }
+        
+        if (!descripcion.trim()){
+            console.log("esta vacio descripcion")
+            return
+        }
+        
+        console.log('procesando datos..' + fruta + descripcion)
+
+        e.target.reset()
+        setFruta('')
+        setDescripcion('')
+    }
+
     return (
         <div>
             <h2>Formulario</h2>
-            <form>
+            <form onSubmit={guardarDatos}>
                 <input
                     type="text"
                     placeholder="Ingresa fruta"
                     className="form-control mb-2"
+                    onChange={e => setFruta(e.target.value)}
                 />
                 <input
                     type="text"
                     placeholder="Ingresa Drescripcion"
                     className="form-control mb-3"
+                    onChange={e => setDescripcion(e.target.value)}
                 />
-                <button className="btn btn-primary btn-block">Agregar</button>
+                <button className="btn btn-primary btn-block" type="submit">Agregar</button>
 
             </form>
         </div>
